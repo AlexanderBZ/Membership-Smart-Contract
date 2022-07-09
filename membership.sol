ab44f3f1acb6a7353d5928414f5a94cb5373b9f3
Alexander Zwerner <Alexander.zwerner@gmail.com>
2022-07-08 21:19:45 -0400

    Update membership.sol

--- membership.sol
+++ membership.sol
@@ -1,15 +1,14 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
 pragma solidity ^0.8.7;
 
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract membership is ERC1155, Ownable {
+contract DFlix is ERC1155, Ownable {
+
     using SafeMath for uint256;
-
-    uint256 public totalPlans;
-
-    constructor(string memory name, string memory symbol, string memory uri) ERC1155(uri) {}
 
     struct plan {
         string name;
@@ -28,41 +27,83 @@ contract membership is ERC1155, Ownable {
 
     mapping(address => subscriber) internal subscribers;
 
+    uint256 public totalPlans;
+
+    constructor(string memory name, string memory symbol, string memory uri) ERC1155(uri) {}
+
+
     modifier correctId(uint id) {
         require(id <= totalPlans && id>0, "provide a correct planID");
         _;
     }
 
+    function setURI(string memory uri) external onlyOwner {
+        _setURI(uri);
+    }
+
+
+
     function ifExpired(uint id) internal view returns(bool) {
         if(subscribers[msg.sender].plan == id) {
-            if((block.timestamp).sub(subscribers[msg.sender].date < plans[id].time)) {
+            if((block.timestamp).sub(subscribers[msg.sender].date) < plans[id].time) {
                 return false;
             } else {
                 return true;
             }
         } else {
             return true;
-            }
         }
-
-    function setURI(string memory uri) external OnlyOwner {
-        _setURI(uri);
     }
 
-    function addPlan(string memory _name, string memory uri, uint256 price, uint time) external OnlyOwner {
+    function addPlan(string memory _name, string memory uri, uint256 price, uint time) external onlyOwner {
         totalPlans = totalPlans.add(1);
         uint256 id = totalPlans.add(1);
-        plans[_id] = plan(_name, uri, 0, price, time);
+        plans[id] = plan(_name, uri, 0, price, time);
     }
 
-    function updatePlan(uint id, string memory _name, string memory uri, uint256 price, uint time) external OnlyOwner {
+    function updatePlan(uint id, string memory _name, string memory uri, uint256 price, uint time) external onlyOwner {
         plans[id] = plan(_name, uri, plans[id].subscribers, price, time);
     }
 
-    function subscribe(uint256 planId) external correctId(planID) payable {
-        require(ifExpired(planId) == true, "your currect plan hasn't expired yet");
+    function subscribe(uint256 planId) external correctId(planId) payable {
+        require(ifExpired(planId) == true, "your current plan hasn't expired yet");
         require(msg.value == plans[planId].price, "please send correct amount of ether");
-        plans[planId].subscribers = (plans[planId].subscribers.add(1));
-        
+        plans[planId].subscribers = (plans[planId].subscribers).add(1);
+        subscribers[msg.sender] = subscriber(planId, block.timestamp);
+        _burn(msg.sender, subscribers[msg.sender].plan, balanceOf(msg.sender, subscribers[msg.sender].plan));
+        plans[subscribers[msg.sender].plan].subscribers = (plans[subscribers[msg.sender].plan].subscribers).sub(balanceOf(msg.sender, subscribers[msg.sender].plan));
+        _mint(msg.sender, planId, 1, "");
+        payable(msg.sender).transfer(msg.value);
+    }
+
+    function currentPlan(address user) public view returns(uint) {
+        require((block.timestamp).sub(subscribers[msg.sender].date) < plans[subscribers[msg.sender].plan].time, "deosn't have any active plan");
+        return subscribers[user].plan;
+    }
+
+    function tokenURI(uint id) public correctId(id) view returns(string memory) {
+        return plans[id].uri;
+    }
+
+    function tokenSupply(uint id) public correctId(id) view returns(uint) {
+        return plans[id].subscribers;
+    }
+
+    function tokenPrice(uint id) public correctId(id) view returns(uint) {
+        return plans[id].price;
+    }
+
+
+    function totalSupply() public view returns(uint) {
+        return totalPlans;
+    }
+
+    function balance() public view onlyOwner returns (uint) {
+        return address(this).balance;
+    }
+
+    function withdraw(uint256 amount) external onlyOwner {
+        (bool success, ) = payable(owner()).call{value: amount}("");
+        require(success, "transfer failed");
     }
 }
